9223eef3aba6f4c34a57d6583a75e919e9c3618a
Williams Henrique <87279572+WilliamsHenrique@users.noreply.github.com>
2023-09-13 02:16:11 -0300

    Tarefa Calculo Media

---
+++ src/br/com/wsliva/NotaMinima.java
@@ -0,0 +1,23 @@
+package br.com.wsliva;
+
+/**
+ * @author Williams.Silva
+ */
+
+public class NotaMinima {
+
+    public static void main(String args[]) {
+
+        int num1 = 9;
+        int num2 = 7;
+        int num3 = 10;
+        int num4 = 8;
+
+        int mediaFinal = (num1 + num2 + num3 + num4) / 4;
+        System.out.println("Resultado Final : " + mediaFinal );
+
+    }
+
+
+
+}
