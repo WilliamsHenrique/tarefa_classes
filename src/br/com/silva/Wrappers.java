db15f5ff21455ad1d6352dbc77a7206e3001e1c7
Williams Henrique <87279572+WilliamsHenrique@users.noreply.github.com>
2023-09-16 17:56:55 -0300

    Tarefa Wrappers

---
+++ src/br/com/silva/Wrappers.java
@@ -0,0 +1,24 @@
+package br.com.silva;
+
+/**
+ * @author Williams.Silva
+ */
+public class Wrappers {
+
+    public static void main(String args[]) {
+        int cod1 = 35486;
+        float cod2 = 1.6636E04f;
+        long codL = 16525896468L;
+
+        Integer numWrappers = cod1;
+        Float num2Wrappers = cod2;
+        Long num3Wrappers = codL;
+
+        System.out.println(cod1);
+        System.out.println(cod2);
+        System.out.println(codL);
+
+    }
+
+}
+
