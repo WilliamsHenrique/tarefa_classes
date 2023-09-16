d588dfd6d8ac2c1c4017f0e6713b83ceee1d45dd
Williams Henrique <87279572+WilliamsHenrique@users.noreply.github.com>
2023-09-16 18:46:25 -0300

    Tarefa ControleDeFluxo

---
+++ src/br/com/wsilvaa/ControleDeFluxo.java
@@ -0,0 +1,28 @@
+package br.com.wsilvaa;
+
+import java.util.Scanner;
+
+public class ControleDeFluxo {
+
+    public static void main(String args[]) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite suas 4 Nota");
+        int nota = s.nextInt();
+        nota = s.nextInt();
+        nota = s.nextInt();
+        nota = s.nextInt();
+        nota = (nota + nota + nota + nota) / 4;
+
+        if ( nota >= 7 ) {
+            System.out.println("Parabéns, você foi aprovado.");
+        } else if ( nota >= 5) {
+            System.out.println("Você está em Recuperação");
+        } else {
+            System.out.println("Infelizmente você foi Reprovado");
+        }
+
+    }
+
+}
